266060f7f55626d46c9101f45c81abea5dde3042
melrwa <orwamelkizedek@gmail.com>
2024-11-01 14:11:01 +0300

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    
+    function handleFOcus(){
+        console.log("Good!")
+    }
+
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    
+    return (
+      <button onFocus={handleFOcus} onBlur={handleBlur}>Eyes on me</button>  
+    );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+ import React from 'react';
 
 function Keypad (){
+    function handleOnchange(event){
+        console.log("Entering password...")
+    } 
     return (
-        <div></div>
+        <div>
+            <form>
+            <input onChange={handleOnchange} type="password" placeholder="input password"/>
+            </form>
+        </div>
     )
 }
 
